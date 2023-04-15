534e298fc3999c1e05ca788ef0a4196f15634c36
Odia Osazuwa Emmanuel <odiaosazuwa@gmail.com>
2023-04-15 04:12:37 -0700

    added a comment to tefel.js

--- tefel.js
+++ tefel.js
@@ -2,6 +2,7 @@ const bar=document.getElementById('bar');
 const close=document.getElementById('close');
 const nav=document.getElementById('navbar');
 
+//To listen to events to make it active
 if(bar){
     bar.addEventListener('click',()=>{
 nav.classList.add('active');
